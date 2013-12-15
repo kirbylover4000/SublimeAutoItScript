d09e671dabba0f7f5241fa5752d12234a8aab0cb
Adam D <kirbylover4000@gmail.com>
2013-12-15 16:37:46 +1030

    use autoit3wrapper from SciTE4AutoIt3

--- autoitbuild.py
+++ autoitbuild.py
@@ -7,41 +7,23 @@ import os
 class autoitbuild(sublime_plugin.WindowCommand):
 	def run(self):
 		filepath = self.window.active_view().file_name()
-		AutoItExePath = sublime.load_settings("AutoIt.sublime-settings").get("AutoItExePath")
-		cmd = [AutoItExePath, "/ErrorStdOut", filepath]
+		AutoIt3WrapperPath = sublime.load_settings("AutoIt.sublime-settings").get("AutoIt3WrapperPath")
+		cmd = [AutoIt3WrapperPath, "/run", "/prod", "/ErrorStdOut", "/in", filepath]
 		self.window.run_command("exec", {"cmd": cmd})
 
 class autoitcompile(sublime_plugin.WindowCommand):
 	def run(self):
 		filepath = self.window.active_view().file_name()
 		AutoItCompilerPath = sublime.load_settings("AutoIt.sublime-settings").get("AutoItCompilerPath")
-		cmd = [AutoItCompilerPath, "/in", filepath]
+		cmd = [AutoItCompilerPath, "/ShowGui", "/in", filepath]
 		self.window.run_command("exec", {"cmd": cmd})
 
-class autoittidy(sublime_plugin.WindowCommand):
+class autoitinfo(sublime_plugin.WindowCommand):
 	def run(self):
-		self.window.run_command("save")
 		filepath = self.window.active_view().file_name()
-		TidyExePath = sublime.load_settings("AutoIt.sublime-settings").get("TidyExePath")
-		tidycmd = [TidyExePath, filepath]
-		try:
-			tidyprocess = subprocess.Popen(tidycmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
-			tidyoutput = tidyprocess.communicate()[0].rstrip()
-			tidyoutputskipfirstline = "".join(tidyoutput.splitlines(True)[1:])
-			self.window.run_command("revert")
-			print("------------ Beginning AutoIt Tidy ------------")
-			print(tidyoutput)
-			if("Tidy Error" in tidyoutput):
-				sublime.active_window().run_command("show_panel", {"panel": "console"})
-				sublime.status_message("### Tidy Errors : Please See Console")
-			else:
-				sublime.status_message(tidyoutputskipfirstline)
-		except Exception as e:
-			sublime.active_window().run_command("show_panel", {"panel": "console"})
-			print("------------ ERROR: Python exception trying to run Tidy ------------")
-			print("TidyCmd was: " + " ".join(tidycmd))
-			print("Error {0}".format(str(e)))
-			sublime.status_message("### EXCEPTION: " + str(e))
+		AutoIt3InfoPath = sublime.load_settings("AutoIt.sublime-settings").get("AutoIt3InfoPath")
+		cmd = [AutoIt3InfoPath]
+		self.window.run_command("exec", {"cmd": cmd})
 
 class autoitincludehelper(sublime_plugin.WindowCommand):
 	def run(self):
